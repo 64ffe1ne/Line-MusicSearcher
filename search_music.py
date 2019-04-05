5f248550f63ff26c9ac15e562f34947aa7ca7a42
yamichang <42953285+64ffe1ne@users.noreply.github.com>
2019-04-05 17:39:09 +0900

    Add files via upload

---
+++ search_music.py
@@ -0,0 +1,30 @@
+import requests as r
+import urllib
+import sys
+import json
+
+
+host = 'https://music-jp-api.line-apps.com/v3/'
+
+lct = ''
+#x-lctを入力
+
+did = ''
+#x-lm-didを入力
+
+sesid = ''
+#生成方法がわからない。アプリを開く度に生成されてるっぽい？
+
+header = {'x-lct': lct, 'Accept': 'application/jsom', 'x-lm-did': did, 'timeout': 30, 'User-Agent': 'LineMusic/3.9.2  (iPhone; U; iOS 12.1.4; ja-JP;)', 'Accept-Language': 'ja-JP;q=1', 'Accept-Encoding': 'br, gzip, deflate', 'Cookie': ''}
+
+query = input("please input keywords: ")
+enc = urllib.parse.quote(query)
+points = host + 'search/title?keyword=' + enc + '&sessionId=' + sesid
+p = r.get(points, headers=header)
+if p.status_code == 200:
+    print(str(p))
+    data = p.json()
+    print(json.dumps(data["result"]["albumList"], indent=4))
+else:
+    p.raise_for_status()
+    print(p.text)
